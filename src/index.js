6065586197bc6eaa782f88c2e2d742e86326d40f
TaahirKolia <87808166+TaahirKolia@users.noreply.github.com>
2024-04-18 09:44:03 +0200

    Add: Deployment Code

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+
+const app = express();
+
+const mainRouter = require('./mainRoutes')
+
+
+app.use(mainRouter)
+
+const port = process.env.PORT || 3000
+app.listen(port)
+console.log('Express server running on port', port)
